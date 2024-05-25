146144b54e8b28dccea77a8409a5e66b067b8c02
AASTHA_2020 <modiaastha0@gmail.com>
2024-05-25 17:29:35 +0530

    done

---
+++ cw2/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='box'>
+     <h1>Learn Web Development </h1>
+     <br />
+     <br />
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit corporis mollitia autem maiores soluta, ducimus non expedita voluptatibus neque quos natus ex totam beatae perferendis officia consectetur est esse.</p>
+     <br />
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit <a href="">voluptatem numquam a earum</a> praesentium, impedit, amet commodi aliquam sint quidem reiciendis voluptate cumque. Natus harum saepe accusantium, dolore maiores quae tenetur labore expedita deserunt laudantium dolorem? Necessitatibus sed, veniam quod deleniti ab recusandae iste reprehenderit! Mollitia dolor quo temporibus suscipit?</p>
+     <br />
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas neque voluptatem quia quaerat repellat. Corporis adipisci non illo voluptatem eum, aspernatur doloremque totam possimus tenetur quidem, laboriosam aliquid. Dolorum cum saepe nesciunt error, sed molestiae amet quo explicabo mollitia quidem.</p>
+     </div>
+    </>
+  )
+}
+
+export default App
